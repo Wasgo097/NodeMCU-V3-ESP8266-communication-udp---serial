931cc513e8945e6c0ad237c2b15d76fe39ae43b1
Wasgo097 <patrykpakula321@gmail.com>
2021-02-27 22:28:21 +0100

    Serial read to buffer

--- source.cpp/source.cpp.ino
+++ source.cpp/source.cpp.ino
@@ -1,9 +1,13 @@
 void setup() {  
   Serial.begin(115200);
 }
-char buffor[256];
+const int buffer_size=255;
+char buffer[buffer_size];
 void loop() {  
   if(Serial.available()>0){
-    
+    int len=Serial.readBytesUntil('\n', buffer, buffer_size);
+    if(len<=buffer_size)buffer[len]='\0';
+    Serial.println(buffer);
+    Serial.flush();
   }
 }
