bcd53230746f816995ba2d216f798b3bd0ca016a
Wasgo097 <patrykpakula321@gmail.com>
2021-02-28 23:03:43 +0100

    Connected serial with wifi

--- source.cpp/source.cpp.ino
+++ source.cpp/source.cpp.ino
@@ -1,51 +1,60 @@
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
-
-const char* ssid = "********";
-const char* password = "********";
-
+#define Debug
+const char* ssid = "ssid";
+const char* password = "password";
 WiFiUDP Udp;
-unsigned int localUdpPort = 4210;  // local port to listen on
-char incomingPacket[255];  // buffer for incoming packets
-char  replyPacket[] = "Hi there! Got the message :-)";  // a reply string to send back
-
-
+const unsigned int localUdpPort = 4210;
+const int buffer_size=255;
+char Buffer[buffer_size]; 
 void setup()
 {
   Serial.begin(115200);
-  Serial.println();
-
   Serial.printf("Connecting to %s ", ssid);
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
+  while (WiFi.status() != WL_CONNECTED)  {
+    delay(1000);
     Serial.print(".");
   }
   Serial.println(" connected");
-
   Udp.begin(localUdpPort);
   Serial.printf("Now listening at IP %s, UDP port %d\n", WiFi.localIP().toString().c_str(), localUdpPort);
 }
-
-
+void FromWiFiToSerial(){
+  int packetSize = Udp.parsePacket();
+  if (packetSize){
+#ifdef Debug
+    Serial.printf("Received %d bytes from %s, port %d\n", packetSize, Udp.remoteIP().toString().c_str(), Udp.remotePort());
+#endif
+    int len = Udp.read(Buffer, buffer_size);
+    if (len > 0)
+      Buffer[len] = 0;
+#ifdef Debug
+    Serial.printf("UDP packet contents: %s\n", Buffer);
+#endif
+#ifdef Debug
+    Serial.write(Buffer);
+#else
+    Serial.println(Buffer);
+#endif
+    Serial.flush();
+  }
+}
+void FromSerialToWifi(){
+    if(Serial.available()>0){
+    int len=Serial.readBytesUntil('\n', Buffer, buffer_size);
+    if(len>0)
+      Buffer[len]='\0';
+#ifdef Debug
+    Serial.println(Buffer);
+#else
+    Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+    Udp.write(Buffer);
+    Udp.endPacket();
+    }
+}
 void loop()
 {
-  int packetSize = Udp.parsePacket();
-  if (packetSize)
-  {
-    // receive incoming UDP packets
-    Serial.printf("Received %d bytes from %s, port %d\n", packetSize, Udp.remoteIP().toString().c_str(), Udp.remotePort());
-    int len = Udp.read(incomingPacket, 255);
-    if (len > 0)
-    {
-      incomingPacket[len] = 0;
-    }
-    Serial.printf("UDP packet contents: %s\n", incomingPacket);
-
-    // send back a reply, to the IP address and port we got the packet from
-    Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
-    Udp.write(replyPacket);
-    Udp.endPacket();
-  }
+  FromWiFiToSerial();
+  FromSerialToWifi();
 }
